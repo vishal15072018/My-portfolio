95bb149d4d6bd388cd3c4b18310639a3976bd23b
Vishal Kumar <vishal15072018@gmail.com>
2026-03-30 08:36:29 +0530

    vite change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path";
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -12,4 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-});
+  build: {
+    outDir: 'dist', // this ensures the build folder is always 'dist'
+  },
+})
