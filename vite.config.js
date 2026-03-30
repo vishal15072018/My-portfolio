15439a2c679a07248e9346da150ded0f46a0f753
Vishal Kumar <vishal15072018@gmail.com>
2026-03-30 08:54:41 +0530

    fix: set absolute dist folder for Render

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path";
+import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -12,6 +11,6 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: 'dist', // this ensures the build folder is always 'dist'
+    outDir: path.resolve(__dirname, 'dist'), // absolute path
   },
 })
